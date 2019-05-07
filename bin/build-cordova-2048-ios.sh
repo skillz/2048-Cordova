b9cc2b886523143fd6466d0d4007437a611e15af
Jerry Lin <jlin@skillz.com>
2019-05-07 13:55:16 -0700

    [ES-1491][ES-2575] Deploy Cordova 2048 to fabric (iOS)

--- bin/build-cordova-2048-ios.sh
+++ bin/build-cordova-2048-ios.sh
@@ -41,3 +41,22 @@ xcodebuild -exportArchive -archivePath "./Cordova-2048.xcarchive" -exportOptions
 zip -y -r Cordova-2048.xcarchive.zip Cordova-2048.xcarchive
 
 cd "${WORKSPACE}"
+
+# Retrieve Fabric API and Build secrets from Sidious
+printf "Retrieving Fabric API and Build secrets from /etc/fabric.properties"
+line_one=$(sed '1q;d' /etc/fabric.properties)
+line_two=$(sed '2q;d' /etc/fabric.properties)
+
+echo ${line_one}
+echo ${line_two}
+
+api_secret=$(echo ${line_one} | cut -d'=' -f2)
+build_secret=$(echo ${line_two} | cut -d'=' -f2)
+
+echo ${api_secret}
+echo ${build_secret}
+
+"${WORKSPACE}/platforms/ios/Crashlytics.framework/submit" ${api_secret} \
+${build_secret} \
+-ipaPath "${WORKSPACE}/Enterprise/Cordova 2048.ipa" \
+-groupAliases SDK,qa-2,tournament-server,product
